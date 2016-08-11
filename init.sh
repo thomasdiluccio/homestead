53daae13e75fbb06a1ac87a9d7e8e2debf6e6e77
Thomas di Luccio <thomas@zenhysteria.fr>
2016-08-11 22:38:25 +0200

    Fix Mongodb forwarding ports automatic configuration

--- init.sh
+++ init.sh
@@ -8,4 +8,13 @@ cp -i vendor/laravel/homestead/src/stubs/Homestead.yaml "$homesteadRoot/Homestea
 cp -i scripts/after.sh "$homesteadRoot/after.sh"
 cp -i vendor/laravel/homestead/src/stubs/aliases "$homesteadRoot/aliases"
 
+cat <<EOT >> "$homesteadRoot/Homestead.yaml"
+
+ports:
+     - send: 3000
+       to: 3000
+     - send: 27017
+       to: 27017
+EOT
+
 echo "Homestead initialized!"
--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -2,7 +2,7 @@
 
 echo "Copying Vagrantfile";
 cp vendor/laravel/homestead/Vagrantfile ./;
-echo "require File.expand_path(File.dirname(__FILE__) + '/scripts/mongodb-forwarding-ports.rb')" >> ./Vagrantfile
+#echo "require File.expand_path(File.dirname(__FILE__) + '/scripts/mongodb-forwarding-ports.rb')" >> ./Vagrantfile
 
 echo "Copying Scripts";
 cp vendor/laravel/homestead/scripts/* ./scripts/;
